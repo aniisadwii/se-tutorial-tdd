f713241dfa07a8341e383258d5cd7f576b5a71a5
saaa <aniisadwii.310@gmail.com>
2025-05-08 12:16:05 +0700

    TDD - REFACTOR

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,20 +1,9 @@
-def add(a, b): 
-    c = a + b;
-    return c
+def add(a, b): return a + b
 
+def subtract(a, b): return a - b
 
-def subtract(a, b):
-    c = a - b;
-    return c
-
-
-def multiply(a, b):
-    c = a * b;
-    return c
-
+def multiply(a, b): return a * b
 
 def divide(a, b):
-    if b == 0:
-        raise ValueError("Division by zero is not allowed")
-    c = a / b;
-    return c
+    if b == 0: raise ValueError("Division by zero is not allowed")
+    return a / b
